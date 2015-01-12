4e55154a9def41e3ffc28d788c25625fcfbf25d9
Vitor Leal <vitorleal1@gmail.com>
2015-01-12 18:17:17 -0200

    add docs area in the pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,18 @@
 (function($) {
+  var $root = $('html, body');
+
+  //On nav click scrool to
+  $('.navbar-nav a').click(function () {
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+
+    }, 600, function () {
+      //Update the url
+      window.location.hash = href;
+    });
+
+    return false;
+  });
 
 })(jQuery);
