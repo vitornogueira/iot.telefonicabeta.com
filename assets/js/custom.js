5a4b4dde7bfeea63aa455e2136ac796f46c231d6
Vitor Leal <vitorleal1@gmail.com>
2015-01-13 18:13:36 -0200

    :rocket: deploy website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,4 +15,45 @@
     return false;
   });
 
+  //Send mail
+  $('.send-button').on('click', function (e) {
+    e.preventDefault();
+
+    var name    = $('#nome').val(),
+        last    = $('#sobrenome').val(),
+        email   = $('#email').val(),
+        message = $('#mensagem').val();
+
+    var message = "Meu nome é " + name + " "+ last +"<"+ email +">, minha mensagem é: " + message + "\n\n";
+
+    if (name && last && email) {
+      $.ajax({
+        url : 'http://iotmail.telefonicabeta.com/send.php',
+        type: 'POST',
+        data: {
+          "from": email,
+          "form_name": name + " " + last,
+          "body": message
+        },
+        success : function(m) {
+          if (m.status) {
+            sweetAlert('Sucesso', 'Mensagem enviada com sucesso', 'success');
+            $('#nome').val(''),
+            $('#sobrenome').val(''),
+            $('#email').val(''),
+            $('#mensagem').val('');
+
+          } else {
+            sweetAlert('Erro', 'Ocorreu um erro ao enviar o email, tente novamente', 'error');
+          }
+        },
+        error: function(m) {
+          sweetAlert('Erro', 'Ocorreu um erro ao enviar o email, tente novamente', 'error');
+        }
+      });
+
+    } else {
+      sweetAlert('Faltam campos obrigatórios', 'Preencha todos os campos para enviar uma mensagem.');
+    }
+  });
 })(jQuery);
