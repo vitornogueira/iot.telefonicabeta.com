1b142171932a0e590a683e80888ad79931f14e86
Vitor Leal <vitorleal1@gmail.com>
2015-02-09 16:21:55 -0200

    :rocket: deploy website

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css');
 
 var libs = [
   'js/libs/jquery.min.js',
@@ -12,10 +13,22 @@ var libs = [
   'js/libs/flickity.js'
 ];
 
+var css = [
+  'css/libs/bootstrap.css',
+  'css/libs/fancybox.css',
+  'css/libs/flickity.css',
+  'css/libs/sweet-alert.css'
+];
+
 gulp.task('default', function() {
   gulp.src(libs)
     .pipe(concat('libs.js'))
     .pipe(uglify())
     .pipe(gulp.dest('js/dist/'));
+
+  gulp.src(css)
+    .pipe(concat('libs.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css/dist/'));
 });
 
